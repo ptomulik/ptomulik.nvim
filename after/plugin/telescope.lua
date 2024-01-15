09a2d850a1c32db33674ebd9b17ecd7751071267
Paweł Tomulik <pawel@tomulik.pl>
2024-01-15 21:20:41 +0100

    workaround for "Telescope stays in insertmode after picker is closed"

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,3 +2,13 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
 
+-- Prevents opening files in insert mode. For more info, see:
+-- https://github.com/nvim-telescope/telescope.nvim/issues/2766#issuecomment-1848930714
+
+vim.api.nvim_create_autocmd("WinLeave", {
+  callback = function()
+    if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+    end
+  end,
+})
