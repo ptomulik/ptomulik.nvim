20e7f90ade81543dbf7c93cf65c91d3a819e8c1d
Paweł Tomulik <pawel@tomulik.pl>
2025-10-26 14:49:51 +0100

    check errors when clonning lasy.nvim repo

--- init.lua
+++ init.lua
@@ -48,8 +48,8 @@ vim.g.maplocalleader = ' '
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local out = vim.fn.system {
     'git',
     'clone',
     '--filter=blob:none',
@@ -57,6 +57,15 @@ if not vim.loop.fs_stat(lazypath) then
     '--branch=stable', -- latest stable release
     lazypath,
   }
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
