323a54ab7d48021e9d7c86cab3df972f898d4248
Paweł Tomulik <pawel@tomulik.pl>
2025-10-26 16:32:56 +0100

    disabled cygwin incompatible plugins

--- init.lua
+++ init.lua
@@ -123,80 +123,80 @@ require('lazy').setup({
 
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim', opts = {} },
-  {
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- See `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- Navigation
-        map({ 'n', 'v' }, ']c', function()
-          if vim.wo.diff then
-            return ']c'
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, desc = 'Jump to next hunk' })
-
-        map({ 'n', 'v' }, '[c', function()
-          if vim.wo.diff then
-            return '[c'
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return '<Ignore>'
-        end, { expr = true, desc = 'Jump to previous hunk' })
-
-        -- Actions
-        -- visual mode
-        map('v', '<leader>hs', function()
-          gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'stage git hunk' })
-        map('v', '<leader>hr', function()
-          gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-        end, { desc = 'reset git hunk' })
-        -- normal mode
-        map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
-        map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
-        map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
-        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-        map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
-        map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
-        map('n', '<leader>hb', function()
-          gs.blame_line { full = false }
-        end, { desc = 'git blame line' })
-        map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
-        map('n', '<leader>hD', function()
-          gs.diffthis '~'
-        end, { desc = 'git diff against last commit' })
-
-        -- Toggles
-        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-        map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
-
-        -- Text object
-        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
-      end,
-    },
-  },
+--  {
+--    -- Adds git related signs to the gutter, as well as utilities for managing changes
+--    'lewis6991/gitsigns.nvim',
+--    opts = {
+--      -- See `:help gitsigns.txt`
+--      signs = {
+--        add = { text = '+' },
+--        change = { text = '~' },
+--        delete = { text = '_' },
+--        topdelete = { text = '‾' },
+--        changedelete = { text = '~' },
+--      },
+--      on_attach = function(bufnr)
+--        local gs = package.loaded.gitsigns
+--
+--        local function map(mode, l, r, opts)
+--          opts = opts or {}
+--          opts.buffer = bufnr
+--          vim.keymap.set(mode, l, r, opts)
+--        end
+--
+--        -- Navigation
+--        map({ 'n', 'v' }, ']c', function()
+--          if vim.wo.diff then
+--            return ']c'
+--          end
+--          vim.schedule(function()
+--            gs.next_hunk()
+--          end)
+--          return '<Ignore>'
+--        end, { expr = true, desc = 'Jump to next hunk' })
+--
+--        map({ 'n', 'v' }, '[c', function()
+--          if vim.wo.diff then
+--            return '[c'
+--          end
+--          vim.schedule(function()
+--            gs.prev_hunk()
+--          end)
+--          return '<Ignore>'
+--        end, { expr = true, desc = 'Jump to previous hunk' })
+--
+--        -- Actions
+--        -- visual mode
+--        map('v', '<leader>hs', function()
+--          gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+--        end, { desc = 'stage git hunk' })
+--        map('v', '<leader>hr', function()
+--          gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+--        end, { desc = 'reset git hunk' })
+--        -- normal mode
+--        map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
+--        map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
+--        map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
+--        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
+--        map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
+--        map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
+--        map('n', '<leader>hb', function()
+--          gs.blame_line { full = false }
+--        end, { desc = 'git blame line' })
+--        map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
+--        map('n', '<leader>hD', function()
+--          gs.diffthis '~'
+--        end, { desc = 'git diff against last commit' })
+--
+--        -- Toggles
+--        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
+--        map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
+--
+--        -- Text object
+--        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
+--      end,
+--    },
+--  },
 
   {
     -- Theme inspired by Atom
@@ -207,19 +207,19 @@ require('lazy').setup({
     end,
   },
 
-  {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
+--  {
+--    -- Set lualine as statusline
+--    'nvim-lualine/lualine.nvim',
+--    -- See `:help lualine.txt`
+--    opts = {
+--      options = {
+--        icons_enabled = false,
+--        theme = 'onedark',
+--        component_separators = '|',
+--        section_separators = '',
+--      },
+--    },
+--  },
 
   {
     -- Add indentation guides even on blank lines
@@ -593,14 +593,14 @@ local servers = {
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
-  lua_ls = {
-    Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
-      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      -- diagnostics = { disable = { 'missing-fields' } },
-    },
-  },
+--  lua_ls = {
+--    Lua = {
+--      workspace = { checkThirdParty = false },
+--      telemetry = { enable = false },
+--      -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+--      -- diagnostics = { disable = { 'missing-fields' } },
+--    },
+--  },
 }
 
 -- Setup neovim lua configuration
