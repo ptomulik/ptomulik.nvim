5a8aa549ec1ef4258acb86bccfb7d442afa4f260
Paweł Tomulik <pawel@tomulik.pl>
2025-11-02 21:13:58 +0100

    fixed typo in init.lua

--- init.lua
+++ init.lua
@@ -617,7 +617,7 @@ mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
   handlers = {
     function(server_name)
-      vim.lsp.config(serve_name, {
+      vim.lsp.config(server_name, {
         capabilities = capabilities,
         settings = servers[server_name],
         filetypes = (servers[server_name] or {}).filetypes,
